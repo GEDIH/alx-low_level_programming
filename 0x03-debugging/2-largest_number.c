4cf9e035b9c0c306f1e5292c9491275496c2acf9
GEDIH <117825147+GEDIH@users.noreply.github.com>
2022-12-14 14:30:58 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-$ amonkeyprogrammer@ubuntu:/debugging$ cat 2-main.c
+carrie@ubuntu:/debugging$ cat 2-main.c
 #include <stdio.h>
 #include "main.h"
 
@@ -22,9 +22,9 @@ int main(void)
 
         return (0);
 }
-
-$ amonkeyprogrammer@ubuntu:/debugging$ cat 2-largest_number.c
-#include "holberton.h"
+carrie@ubuntu:/debugging$
+carrie@ubuntu:/debugging$ cat 2-largest_number.c
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -54,6 +54,8 @@ int largest_number(int a, int b, int c)
     return (largest);
 }
 
-$ amonkeyprogrammer@ubuntu:/debugging$
-
-
+carrie@ubuntu:/debugging$
+carrie@ubuntu:/debugging$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 2-largest_number.c 2-main.c -o 2-main
+carrie@ubuntu:/debugging$ ./2-main
+0 is the largest number
+carrie@ubuntu:/debugging$
