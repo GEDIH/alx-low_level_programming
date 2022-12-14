9cf5a693dd4840f08d113c79d31b66c2b7a8755b
GEDIH <117825147+GEDIH@users.noreply.github.com>
2022-12-14 14:22:37 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -22,4 +22,38 @@ int main(void)
 
         return (0);
 }
+
+$ amonkeyprogrammer@ubuntu:/debugging$ cat 2-largest_number.c
+#include "holberton.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
+
 $ amonkeyprogrammer@ubuntu:/debugging$
+
+
