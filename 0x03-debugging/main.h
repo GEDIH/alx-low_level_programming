a0ed9ea7d9ad293a3fdea2f31b8da2074a821dee
GEDIH <117825147+GEDIH@users.noreply.github.com>
2022-12-15 16:41:26 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 
 #include <stdio.h>
 
@@ -8,8 +6,4 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 
 void print_remaining_days(int month, int day, int year);
-
-
 int convert_day(int month, int day);
-
-#endif
