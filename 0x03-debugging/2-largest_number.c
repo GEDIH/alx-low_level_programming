4abb5ae78368ba3fd038c14f9afc40c7a5c6b67d
GEDIH <117825147+GEDIH@users.noreply.github.com>
2022-12-15 16:36:16 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,29 +1,3 @@
-carrie@ubuntu:/debugging$ cat 2-main.c
-#include <stdio.h>
-#include "main.h"
-
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
-carrie@ubuntu:/debugging$
-carrie@ubuntu:/debugging$ cat 2-largest_number.c
 #include "main.h"
 
 /**
@@ -36,26 +10,25 @@ carrie@ubuntu:/debugging$ cat 2-largest_number.c
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
 }
 
-carrie@ubuntu:/debugging$
-carrie@ubuntu:/debugging$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 2-largest_number.c 2-main.c -o 2-main
-carrie@ubuntu:/debugging$ ./2-main
-0 is the largest number
-carrie@ubuntu:/debugging$
+
+return (largest);
+}
