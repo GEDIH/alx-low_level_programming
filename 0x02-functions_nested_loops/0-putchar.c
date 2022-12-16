4b16ebb99be5c67f1d4a55497768af7c691b63e7
GEDIH <117825147+GEDIH@users.noreply.github.com>
2022-12-16 09:49:23 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-	char GEDIH[] = "_putchar";
+	char gedih[] = "_putchar";
 
 	int c;
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(abe[c]);
+		_putchar(gedih[c]);
 	}
 	_putchar('\n');
 	return (0);
