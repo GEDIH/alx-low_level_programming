0a7416a91d04f4be3429e2356a5c0d89ac1e3cf8
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-03-22 11:08:56 +0300

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -wall -wextra -werror -pedantic -c -fPIC *.C
+gcc -wall -wextra -werror -pedantic -c -fPIC *.c
 gcc -shared -0 liball.so *.0
 export LD_LIBRARY_PATH=.:SLD_LIBRARY_PATH
